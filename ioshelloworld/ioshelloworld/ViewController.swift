1ea3d25e69882abc854d45540cc4f58f828f0b54
tomson <tomson@tomsontekimacbook-pro.local>
2016-09-13 16:12:05 +0800

    one push

---
+++ ioshelloworld/ioshelloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ioshelloworld
+//
+//  Created by tomson on 16/9/13.
+//  Copyright © 2016年 tomson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
